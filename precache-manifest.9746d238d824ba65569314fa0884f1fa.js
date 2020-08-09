38f501e9470ae9826f7643c32f6c39af6647f973
jake <jake@jakecoffman.com>
2020-08-09 13:32:14 -0500

    deploy

---
+++ precache-manifest.9746d238d824ba65569314fa0884f1fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faef38b5120c4c4df694",
+    "url": "/colors/css/app.7c01098d.css"
+  },
+  {
+    "revision": "42fd4441b2a2c493780efebb30a949f3",
+    "url": "/colors/index.html"
+  },
+  {
+    "revision": "faef38b5120c4c4df694",
+    "url": "/colors/js/app.5b82bb40.js"
+  },
+  {
+    "revision": "2c2125b6739761b8b819",
+    "url": "/colors/js/chunk-vendors.6bd3d473.js"
+  },
+  {
+    "revision": "0f2e49d86a7d8ebb171b296c9ece993d",
+    "url": "/colors/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/colors/robots.txt"
+  }
+]);
